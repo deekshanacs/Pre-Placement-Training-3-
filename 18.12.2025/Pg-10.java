a79f2b572c5a42e2c96d17c2b4d9596b2a410e17
Deekshana C S <145849222+deekshanacs@users.noreply.github.com>
2025-12-18 19:52:57 +0530

    Implement LinkedListCycle detection algorithm

---
+++ 18.12.2025/Pg-10.java
@@ -0,0 +1,16 @@
+class ListNode {
+    int val;
+    ListNode next;
+}
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;         
+            fast = fast.next.next;   
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
