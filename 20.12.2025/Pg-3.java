19281718aee32470cc88c8aa250aecffa7527457
Deekshana C S <145849222+deekshanacs@users.noreply.github.com>
2025-12-20 21:22:25 +0530

    Create Pg-3.java

---
+++ 20.12.2025/Pg-3.java
@@ -0,0 +1,13 @@
+
+int left = 0, right = 1;
+int maxProfit = 0;
+
+while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+        int profit = prices[right] - prices[left];
+        if (profit > maxProfit) maxProfit = profit;
+    } else {
+        left = right; 
+    }
+    right++;
+}
