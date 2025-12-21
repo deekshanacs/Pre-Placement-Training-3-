8cf67d71e739ebe5d3f7b005be3441e7eca7fe3d
Deekshana C S <145849222+deekshanacs@users.noreply.github.com>
2025-12-21 19:33:11 +0530

    Create Pg-7.java

---
+++ 21.12.2025/Pg-7.java
@@ -0,0 +1,30 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
+
+public class BSTDemo {
+    public static void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.val + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(50);
+        root.left = new TreeNode(30);
+        root.right = new TreeNode(70);
+        root.left.left = new TreeNode(20);
+        root.left.right = new TreeNode(40);
+
+        System.out.print("Sorted elements (Inorder): ");
+        inorder(root);
+    }
+}
