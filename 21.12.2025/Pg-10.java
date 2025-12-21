dfff42b9a7974749a4007f1570d5aa9c019d0bda
Deekshana C S <145849222+deekshanacs@users.noreply.github.com>
2025-12-21 19:36:17 +0530

    Create Pg-10.java

---
+++ 21.12.2025/Pg-10.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class GraphBFS {
+    private int V; 
+    private LinkedList<Integer> adj[];
+
+    GraphBFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            for (int n : adj[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        GraphBFS g = new GraphBFS(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Breadth First Traversal (starting from vertex 2):");
+        g.BFS(2);
+    }
+}
