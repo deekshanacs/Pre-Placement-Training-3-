9cbf24cd9330e069913a18e25cdf6e60e4d7ea05
Deekshana C S <145849222+deekshanacs@users.noreply.github.com>
2026-01-11 18:04:13 +0530

    Create Pg-7.py

---
+++ 11.01.2026/Pg-7.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3)
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+print(f"Model Accuracy: {accuracy_score(y_test, predictions) * 100:.2f}%")
